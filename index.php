4b21d8286f61fa5c7ae6c34685ba5216a4c8679d
Stefania9797 <stefaniamastrel@gmail.com>
2021-06-25 14:59:33 +0200

    aggiunto metodo e istanze

--- index.php
+++ index.php
@@ -6,16 +6,30 @@ class Movie{
     public $plot;
     public $language;
     public $director;
-    public $rating;
+    public $price;
     //all'interno della classe è definito un costruttore
-    public function __construct($_title, $_plot, $_language, $_director, $_rating) {
+    public function __construct($_title, $_plot, $_language, $_director,$_price) {
         $this->title = $_title;
         $this->plot = $_plot;
         $this->language = $_language;
         $this->director = $_director;
-        $this->rating = $_rating;
+        $this->price = $_price;
     }
-}
-//all'interno della classe è definito almeno un metodo
+    //all'interno della classe è definito almeno un metodo
+    public function getPrice()
+    {
+        return $this->price;
+    }
+    public function setPrice(int $price)
+    {
+        $this->price = $price;
+    }
+
+};
 //vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà
+$titanic = new Movie("Titanic","lorem ipsum dolor sit","Emglish","James Cameron",12.99);
+$inception = new Movie("Inception","lorem ipsum dolor sit","Emglish","Christopher Nolan",30);
+$pinocchio = new Movie("Pinocchio","lorem ipsum dolor sit","Italiano","Matteo Garrone",10);
+
+$titanic->setPrice(10);
 ?>
